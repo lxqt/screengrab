c9de832fa9f1cf9b05ea06845cd7e0f0790e4382
DOOMer <doomer3d@gmail.com>
2013-09-22 10:19:32 +0400

    Add empty widget for MediaCrush uploading settings. Fixed #36

---
+++ src/modules/uploader/mediacrush/uploaderconfigwidget_mediacrush.h
@@ -0,0 +1,22 @@
+#ifndef UPLOADERCONFIGWIDGET_MEDIACRUSH_H
+#define UPLOADERCONFIGWIDGET_MEDIACRUSH_H
+
+#include <QWidget>
+
+namespace Ui {
+class UploaderConfigWidget_MediaCrush;
+}
+
+class UploaderConfigWidget_MediaCrush : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit UploaderConfigWidget_MediaCrush(QWidget *parent = 0);
+    ~UploaderConfigWidget_MediaCrush();
+
+private:
+    Ui::UploaderConfigWidget_MediaCrush *ui;
+};
+
+#endif // UPLOADERCONFIGWIDGET_MEDIACRUSH_H
