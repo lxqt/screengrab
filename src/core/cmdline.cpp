f1854088401bfa7abde726990f3d4e5a4c766706
Jerome Leclanche <jerome@leclan.ch>
2014-04-14 02:12:53 +0100

    Clean up --help output

--- src/core/cmdline.cpp
+++ src/core/cmdline.cpp
@@ -34,7 +34,7 @@ CmdLine::~CmdLine()
 }
 
 void CmdLine::registerParam(const QString& param, const QString& description, CmdLineParam::CmdLineParam paramType)
-{	
+{
 	switch(paramType)
 	{
 		case CmdLineParam::ScreenType:
@@ -55,7 +55,7 @@ void CmdLine::registerParam(const QString& param, const QString& description, Cm
 		default:
 			break;
 	}
-	
+
 	_regstredParams.insert(param, description);
 }
 
@@ -70,15 +70,15 @@ void CmdLine::parse()
     for (int i= 1; i != QApplication::argc(); ++i)
     {
 		param = QApplication::argv()[i];
-		
+
 		if (param.startsWith("--") != true)
 		{
 			_invalidParams << param;
 		}
-		else 
+		else
 		{
 			param = param.remove(0, 2);
-			
+
 			if (_regstredParams.contains(param) == true)
 			{
 				_usedParams << param;
@@ -89,13 +89,13 @@ void CmdLine::parse()
 			}
 		}
     }
-    
+
     if (_invalidParams.count() > 0)
 	{
 		QString printable;
 		for (int i =0; i < _invalidParams.count(); ++i)
 		{
-			printable = "'" + _invalidParams.at(i) + "' is incorrect command line param.";
+			printable = "unrecognized command line option: '" + _invalidParams.at(i) + "'";
 			print(printable);
 		}
 	}
@@ -115,38 +115,36 @@ qint8 CmdLine::selectedScreenType()
 
 void CmdLine::printHelp()
 {
-	print("ScreenGrab usage:");
-	print("\nscreengrub [screentype] [additional param]");
-	
+	print("usage: screengrab [options] [screenshot type]");
+
 	QString optStr;
-	
-	print("\nscreenshot type command line params:");
-		
+
+	print("\nScreenshot types:");
+
 	for (int i = 0; i < _screenTypeParams.count(); ++i)
 	{
-		optStr += "--" + _screenTypeParams.at(i) + "\t" + _regstredParams.value(_screenTypeParams.at(i));
+		optStr += "  --" + _screenTypeParams.at(i) + "\t" + _regstredParams.value(_screenTypeParams.at(i));
 		print(optStr);
 		optStr.clear();
 	}
-	
-	print("\nadditional command line params:");
-		
+
+	print("\nStartup behaviour:");
+
 	for (int i = 0; i < _utilityParams.count(); ++i)
 	{
-		optStr += "--" + _utilityParams.at(i) + "\t" + _regstredParams.value(_utilityParams.at(i));
+		optStr += "  --" + _utilityParams.at(i) + "\t" + _regstredParams.value(_utilityParams.at(i));
 		print(optStr);
 		optStr.clear();
 	}
-	
-	print("\nMisc params:");
-		
+
+	print("\nMiscellaneous:");
+
 	for (int i = 0; i < _onlyPrintParams.count(); ++i)
 	{
-		optStr += "--" + _onlyPrintParams.at(i) + "\t" + _regstredParams.value(_onlyPrintParams.at(i));
+		optStr += "  --" + _onlyPrintParams.at(i) + "\t" + _regstredParams.value(_onlyPrintParams.at(i));
 		print(optStr);
 		optStr.clear();
 	}
-	
 }
 
 void CmdLine::print(const QString& string)
