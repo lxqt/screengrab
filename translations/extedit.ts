e66d211c98347b5561c1ac16013b89ecd0349fcc
Luís Pereira <luis.artur.pereira@gmail.com>
2014-12-11 04:26:19 -0800

    Extracts modules translations sources

---
+++ translations/extedit.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/modules/extedit/moduleextedit.cpp" line="40"/>
+        <source>External edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/extedit/moduleextedit.cpp" line="51"/>
+        <source>Edit in...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/uploader.ts
@@ -0,0 +1,257 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DialogUploader</name>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="14"/>
+        <source>Upload to internet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="22"/>
+        <source>Upload to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="76"/>
+        <source>Direct link:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="98"/>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="189"/>
+        <source>Open this link in the default web-browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="101"/>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="192"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="114"/>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="155"/>
+        <source>Copy this link to the clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="117"/>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="158"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="128"/>
+        <source>Extended preformed html or bb codes:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="167"/>
+        <source>Link to delete image:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="300"/>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.ui" line="313"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="66"/>
+        <source>Size: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="66"/>
+        <source> pixel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="76"/>
+        <source>Uploaded </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="79"/>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="238"/>
+        <source>Ready to upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="119"/>
+        <source>Upload processing... Please wait</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="183"/>
+        <source>Receiving a response from the server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="201"/>
+        <source>Upload completed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="225"/>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="240"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="232"/>
+        <source>Error uploading screenshot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="233"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="274"/>
+        <source>Open this link in your default web-browser, it may directly delete your uploaded image, without any warnings.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/dialoguploader.cpp" line="275"/>
+        <source>Are you sure you want to continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ModuleUploader</name>
+    <message>
+        <location filename="../src/modules/uploader/moduleuploader.cpp" line="47"/>
+        <source>Uploading</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/modules/uploader/moduleuploader.cpp" line="101"/>
+        <source>Upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Uploader</name>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="228"/>
+        <source>Direct link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="231"/>
+        <source>HTML code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="234"/>
+        <source>BB code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="237"/>
+        <source>HTML code with thumb image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="240"/>
+        <source>BB code with thumb image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploader.cpp" line="243"/>
+        <source>URl to delete image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UploaderConfigWidget</name>
+    <message>
+        <location filename="../src/modules/uploader/uploaderconfigwidget.ui" line="24"/>
+        <source>Common settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploaderconfigwidget.ui" line="32"/>
+        <source>Default image host</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploaderconfigwidget.ui" line="44"/>
+        <source>Always copy the link to the clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploaderconfigwidget.ui" line="65"/>
+        <source>Hosts settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/uploaderconfigwidget.ui" line="73"/>
+        <source>Settings for: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UploaderConfigWidget_ImgUr</name>
+    <message>
+        <location filename="../src/modules/uploader/imgur/uploaderconfigwidget_imgur.ui" line="20"/>
+        <source>Configuration for imgur.com upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/imgur/uploaderconfigwidget_imgur.ui" line="43"/>
+        <source>Now is nothing yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UploaderConfigWidget_MediaCrush</name>
+    <message>
+        <location filename="../src/modules/uploader/mediacrush/uploaderconfigwidget_mediacrush.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/mediacrush/uploaderconfigwidget_mediacrush.ui" line="20"/>
+        <source>Configuration for mediacru.sh upload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/mediacrush/uploaderconfigwidget_mediacrush.ui" line="43"/>
+        <source>Now is nothing yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Uploader_ImgUr_Widget</name>
+    <message>
+        <location filename="../src/modules/uploader/imgur/uploader_imgur_widget.ui" line="20"/>
+        <source>Upload to ImgUr.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Uploader_MediaCrush_Widget</name>
+    <message>
+        <location filename="../src/modules/uploader/mediacrush/uploader_mediacrush_widget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/modules/uploader/mediacrush/uploader_mediacrush_widget.ui" line="20"/>
+        <source>Upload to MediaCrush</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
