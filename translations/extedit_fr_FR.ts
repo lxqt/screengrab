b272b61f4bade7fe00dfd740e365bf3ddec738f2
Thibaut Quentin <thibaut.quentin@laposte.net>
2017-08-06 17:19:00 +0200

    French translation

---
+++ translations/extedit_fr_FR.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/modules/extedit/moduleextedit.cpp" line="38"/>
+        <source>External edit</source>
+        <translation>Édition externe</translation>
+    </message>
+    <message>
+        <location filename="../src/modules/extedit/moduleextedit.cpp" line="49"/>
+        <source>Edit in...</source>
+        <translation>Éditer avec...</translation>
+    </message>
+</context>
+</TS>
