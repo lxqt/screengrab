c14e1b7aaf7531b72363b4eb053400b5cad1de4b
Miguel A. Bouzada <mbouzada@gmail.com>
2018-12-12 11:21:10 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (181 of 181 strings)
    
    Translation: LXQt/ScreenGrab
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/screengrab/gl/

--- translations/screengrab_gl.ts
+++ translations/screengrab_gl.ts
@@ -455,7 +455,7 @@
     <message>
         <location filename="../src/core/ui/mainwindow.ui" line="246"/>
         <source>toolBar</source>
-        <translation>Barra de ferramentas</translation>
+        <translation>BarraDeFerramentas</translation>
     </message>
     <message>
         <location filename="../src/core/ui/mainwindow.cpp" line="64"/>
