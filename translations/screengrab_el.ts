b6a36db7556013f9191afacd8bddb12f4d00c743
Weblate <noreply@weblate.org>
2022-11-21 07:10:40 +0100

    Weblate commits
    Co-authored-by: Andrei Stepanov <adem4ik@gmail.com>
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/ru/
    Translation: LXQt Desktop/ScreenGrab

--- translations/screengrab_el.ts
+++ translations/screengrab_el.ts
@@ -31,17 +31,17 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="34"/>
         <source>using Qt %1</source>
-        <translation type="unfinished"></translation>
+        <translation>χρήση της Qt %1</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="105"/>
         <source>Website - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Ιστοσελίδα - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="108"/>
         <source>Licensed under the &lt;a href=%1&gt;GPL v2&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Υπόκειται στην άδεια χρήσης &lt;a href=%1&gt;GPL v2&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="112"/>
