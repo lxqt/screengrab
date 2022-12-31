2c7c5ff5f518a6fd85facd9b646860691d5c1bcd
Weblate <noreply@weblate.org>
2022-12-31 04:10:55 +0100

    Weblate commits
    Co-authored-by: Joan Miró <miro.oro.jm@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/ca/
    Translation: LXQt Desktop/ScreenGrab

--- translations/screengrab_ca.ts
+++ translations/screengrab_ca.ts
@@ -31,17 +31,17 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="34"/>
         <source>using Qt %1</source>
-        <translation type="unfinished"></translation>
+        <translation>usant les Qt %1</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="105"/>
         <source>Website - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Lloc web - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="108"/>
         <source>Licensed under the &lt;a href=%1&gt;GPL v2&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Llicencia sota el &lt;a href=%1&gt;GPL v2&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="112"/>
