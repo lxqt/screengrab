4ad8277c97efc19ff29ef84b55d73c32b372d830
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-03-27 03:27:39 +0800

    Weblate commits (#329)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/lt/
    Translation: LXQt Desktop/ScreenGrab
    
    Co-authored-by: Moo <hazap@hotmail.com>

--- translations/screengrab_lt.ts
+++ translations/screengrab_lt.ts
@@ -31,17 +31,17 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="34"/>
         <source>using Qt %1</source>
-        <translation type="unfinished"></translation>
+        <translation>naudoja Qt %1</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="105"/>
         <source>Website - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Internetinė svetainė – &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="108"/>
         <source>Licensed under the &lt;a href=%1&gt;GPL v2&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Licencijuota pagal &lt;a href=%1&gt;GPL v2&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="112"/>
