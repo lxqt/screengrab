1431d3ebc30ba93bd3f3ffe8bbfe2b1f0096ee47
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2023-05-05 03:46:56 +0800

    Weblate commits (#333)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/sk/
    Translation: LXQt Desktop/ScreenGrab
    
    Co-authored-by: Paolo Vigoroso <xpaolox@zoznam.sk>

--- translations/screengrab_sk.ts
+++ translations/screengrab_sk.ts
@@ -31,17 +31,17 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="34"/>
         <source>using Qt %1</source>
-        <translation type="unfinished"></translation>
+        <translation>pomocou Qt %1</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="105"/>
         <source>Website - &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Webové stránky – &lt;a href=&quot;%1&quot;&gt;%1&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="108"/>
         <source>Licensed under the &lt;a href=%1&gt;GPL v2&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Pod licenciou &lt;a href=%1&gt;GPL v2&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="112"/>
