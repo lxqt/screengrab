22aa0c7c4ee00bbef55be7e50435d0fa2abb0747
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2026-03-17 07:58:25 +0100

    Weblate commits (#456)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/da/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/screengrab/en_GB/
    Translation: LXQt Desktop/ScreenGrab
    
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: Peter  Jespersen <flywheel@illogical.dk>

--- translations/screengrab_da.ts
+++ translations/screengrab_da.ts
@@ -51,7 +51,7 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="113"/>
         <source>Copyright &amp;copy; 2013-2025, LXQt team</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Copyright &amp;copy; 2013-2025, LXQt team</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="120"/>
@@ -86,17 +86,17 @@
     <message>
         <location filename="../src/core/ui/about.cpp" line="142"/>
         <source>Special thanks to:</source>
-        <translation type="unfinished"></translation>
+        <translation>En speciel tak til:</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="144"/>
         <source>for creating ScreenGrab</source>
-        <translation type="unfinished"></translation>
+        <translation>til at skabe ScreenGrab</translation>
     </message>
     <message>
         <location filename="../src/core/ui/about.cpp" line="145"/>
         <source>for supporting wlr screencopy protocol</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">for understøttelse af WLR-skærmkopieringsprotokollen</translation>
     </message>
 </context>
 <context>
@@ -173,7 +173,7 @@
     <message>
         <location filename="../src/core/core.cpp" line="85"/>
         <source>Save screenshot and exit, without showing window</source>
-        <translation type="unfinished"></translation>
+        <translation>Gem skærmbillede og afslut uden at vise vinduet</translation>
     </message>
     <message>
         <location filename="../src/core/core.cpp" line="587"/>
@@ -216,7 +216,7 @@
     <message>
         <location filename="../src/core/ui/mainwindow.ui" line="73"/>
         <source>Screen:</source>
-        <translation type="unfinished"></translation>
+        <translation>Skærm:</translation>
     </message>
     <message>
         <location filename="../src/core/ui/mainwindow.ui" line="105"/>
@@ -361,7 +361,10 @@
 double click or press Enter
 to take a screenshot,
 or press Escape to cancel.</source>
-        <translation type="unfinished"></translation>
+        <translation>Klik og træk, for at tegne et rektangel,
+dobbeltklik eller tryk på Enter
+for at tage et skærmbillede,
+eller tryk på Escape for at annullere.</translation>
     </message>
     <message>
         <location filename="../src/core/regionselect.cpp" line="244"/>
@@ -579,22 +582,22 @@ måske blive gjort større for at passe til yderkanterne</translation>
     <message>
         <location filename="../src/core/ui/configwidget.ui" line="376"/>
         <source>With multiple screens</source>
-        <translation type="unfinished"></translation>
+        <translation>Med flere skærme</translation>
     </message>
     <message>
         <location filename="../src/core/ui/configwidget.ui" line="379"/>
         <source>Remember last selected screen in next session</source>
-        <translation type="unfinished"></translation>
+        <translation>Husk sidst valgte skærm i næste session</translation>
     </message>
     <message>
         <location filename="../src/core/ui/configwidget.ui" line="386"/>
         <source>Notify on saving and copying</source>
-        <translation type="unfinished"></translation>
+        <translation>Underret ved lagring og kopiering</translation>
     </message>
     <message>
         <location filename="../src/core/ui/configwidget.ui" line="395"/>
         <source>Duration:</source>
-        <translation type="unfinished"></translation>
+        <translation>Varighed:</translation>
     </message>
     <message>
         <location filename="../src/core/ui/configwidget.ui" line="462"/>
