0e2bc5f07e87f524f6adc3ce7937888b53bb4775
stefonarch <standreas@riseup.net>
2026-03-25 21:59:32 +0100

    Silenced layershell warning

--- src/core/regionselect.cpp
+++ src/core/regionselect.cpp
@@ -128,7 +128,7 @@ void RegionSelect::showEvent(QShowEvent *event)
                 layershell->setKeyboardInteractivity(LayerShellQt::Window::KeyboardInteractivityExclusive);
                 layershell->setExclusiveZone(-1); // not moved to accommodate for other surfaces
                 win->setScreen(_selectedScreen == nullptr ? qApp->primaryScreen() : _selectedScreen);
-                layershell->setScreenConfiguration(LayerShellQt::Window::ScreenConfiguration::ScreenFromQWindow);
+                layershell->setWantsToBeOnActiveScreen(true);
             }
         }
     }
